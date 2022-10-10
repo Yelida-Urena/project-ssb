1b5b1182f0981a969517400f06b8f162abc023ad
Yelida <urena@infox.de>
2022-10-10 16:18:18 -0400

    Adding components

--- smart-buoy-2/src/app/app.module.ts
+++ smart-buoy-2/src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { ReportsComponent } from './reports/reports.component';
+import { DevicesComponent } from './devices/devices.component';
+import { FormsComponent } from './forms/forms.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    StartPageComponent,
+    ReportsComponent,
+    DevicesComponent,
+    FormsComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
